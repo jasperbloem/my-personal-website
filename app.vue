c43551f6097f49e7596e5cf54a8846686f71602a
Jasper Bloem <20337391+jasperbloem@users.noreply.github.com>
2023-05-17 14:28:17 +0200

    Added cookie bar & functionality to block GA cookies

--- app.vue
+++ app.vue
@@ -1,5 +1,32 @@
 <template>
+    <CookieControl v-bind:locale="locale" />
     <NuxtLayout>
         <NuxtPage />
     </NuxtLayout>
 </template>
+
+<script setup lang="ts">
+const {
+    cookiesEnabled,
+    cookiesEnabledIds,
+    isConsentGiven,
+    isModalActive,
+    moduleOptions
+} = useCookieControl()
+
+const { locale } = useI18n()
+
+watch(
+    () => cookiesEnabledIds.value,
+    (current, previous) => {
+        if (
+            (!previous?.includes('_ga') &&
+                current?.includes('_ga'))
+        ) {
+            // cookie with id `google-analytics` got added
+            window.location.reload() // placeholder for your custom change handler
+        }
+    },
+    { deep: true }
+)
+</script>
--- nuxt.config.js
+++ nuxt.config.js
@@ -12,7 +12,50 @@ export default defineNuxtConfig({
     modules: [
         '@nuxtjs/color-mode',
         '@nuxtjs/i18n',
+        '@dargmuesli/nuxt-cookie-control'
     ],
+    cookieControl: {
+        colors: {
+            checkboxActiveBackground: '#267494',
+            modalButtonBackground: '#267494',
+            barButtonHoverBackground: '#267494',
+            controlButtonBackground: '#fff',
+            controlButtonHoverBackground: '#c8c8c8',
+            controlButtonIconColor: '#000',
+            controlButtonIconHoverColor: '#373737',
+        },
+        closeModalOnClickOutside: true,
+        cookies: {
+            necessary: [
+                {
+                    id: '',
+                    name: 'Functional cookies',
+                    description: '',
+                    default: true,
+                },
+            ],
+            optional: [
+                {
+                    id: '_ga',
+                    name: 'Google Analytics',
+                    description: {
+                        nl: 'Wordt gebruikt om website-analyses bij te houden',
+                        en: 'Used to track website analytics',
+                    },
+                    default: true,
+                },
+            ],
+        },
+        locales: ['en', 'nl'],
+        localeTexts: {
+            nl: {
+                bannerDescription: 'Deze website gebruikt functionele en GA cookies om het gebruik te meten. Wil je hiermee akkoord gaan?'
+            },
+            en: {
+                bannerDescription: 'This website uses functional and GA cookies to measure usage. Do you agree?'
+            }
+        }
+    },
     i18n: {
         locales: [
             {
